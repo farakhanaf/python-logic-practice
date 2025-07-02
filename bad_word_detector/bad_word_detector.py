b3835b0f52df6c569eaca724909a587dea132786
Tiar Salsabila Fitriazaki <tiarsalsa@gmail.com>
2025-07-02 14:22:40 +0700

    keterangan tambahan

--- bad_word_detector/bad_word_detector.py
+++ bad_word_detector/bad_word_detector.py
@@ -18,21 +18,23 @@ I do have skill issues since long time ago, but my bosses told me to not giving 
 # 5. Bila tidak ada kata yang terdeteksi maka inputan akan dimunculkan di program
 
 # 1. Buat list kata" yang dilarang
-bad_words = ['bodoh', 'bangsat', 'anjing', 'kontol', 'goblok', 'tai', 'nigga']
+bad_words = ['bodoh', 'bangsat', 'anjing', 'kontol', 'goblok', 'tai', 'nigga', 'hitler']
 
 # 2. Buat inputan user ke dalam programnya
 input_kalimat = input('Chat: ')
 
 # 3. Buat function yang mendeteksi inputan dengan list kata" yang dilarang
+# variable 'teks' itu bebas dalam artian ketika memanggil function ini dengan variable lainnya maka variable baru tersebut sama dengan variable teks (semoga mengerti, bisa ditanyakan lagi kalau masih bingung urutannya ya)
 def detect_bad_word(teks):
     # Pertama, konversi dulu kata yang masuk menjadi kecil
     teks = teks.lower()
     
     # 4. Bila terdeteksi kata terlarang di dalam inputan, maka beri keterangan 'Banned'
+    # Cek/Loop untuk setiap variable 'kata' di dalam list 'bad_words'
     for kata in bad_words:
-        # Dilakukan pengecekan untuk setiap value di dalam list/dict
+        # Jika kata ada yang cocok dengan list di variable teks maka
         if kata in teks:
-            print('Maaf, anda di banned')
+            print('Mohon untuk menghindari kata" yang dilarang')
             return
     
     # 5. Bila tidak ada kata yang terdeteksi maka inputan akan dimunculkan di program
