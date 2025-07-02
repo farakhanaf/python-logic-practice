8e51b703eba3f8334b06e0deb364612b94bb3392
Tiar Salsabila Fitriazaki <tiarsalsa@gmail.com>
2025-07-02 20:04:13 +0700

    user_data_app update

---
+++ user_data_app/user_data_app.py
@@ -0,0 +1,52 @@
+'''
+KETERANGAN SMALL PROJECT: USER_DATA_APP
+
+IDE:
+- Form validasi lengkap (pakai regex)
+- Aplikasi input data banyak user
+- Simpan ke file .csv
+
+PERPADUAN:
+- Validasi email
+- Input banyak user
+- Simpan data ke file .csv
+
+TUJUAN:
+User bisa input:
+- Nama
+- Email
+
+Aplikasi akan:
+- Validasi email
+- Simpan data ke file CSV jika valid
+'''
+
+
+# Pertama" import csv karena hasil dari inputan akan disimpan ke file csv tersebut
+import csv
+
+# Buat function sederhana untuk validasi email
+def email_validation(email):
+    if email.endswith("@gmail.com"):
+        print(email,"is valid a email address")
+        return True
+    else:
+        print(email,"is invalid email address")
+        return False
+    
+# Buka (atau buat) file CSV untuk menyimpan data
+with open("user_data_app/user_data.csv", "a", newline="") as file:
+    writer = csv.writer(file)
+    
+    while True:
+        name = input("Enter your name (or type 'exit' to quit): ")
+        if name.lower() == 'exit':
+            break
+        
+        email = input("Enter your email: ")
+        
+        if email_validation(email):
+            writer.writerow([name, email])
+            print("✅ Data saved.")
+        else:
+            print("❌ Invalid email. Data not saved.")
